bbcf55070f7b09c71bdae0b65a54be547e73c385
jkimkim <kimjohnklm21@gmail.com>
2022-03-13 01:41:11 +0300

    add files

---
+++ public/projects/js/deck/deck.js
@@ -0,0 +1,6 @@
+var suits = ['C', 'D', 'S', 'H']
+var ranks = ['1', '2', '3', '4', '5','6','7','8','8','10','J','Q','K','A']
+
+for (suitCounter = 0; suitCounter < 4; suitCounter++){
+    console.log(suitCounter)
+}
