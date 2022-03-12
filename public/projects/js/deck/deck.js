b82c40f8876368047bbf098ac645dd4cd5d687c8
jkimkim <kimjohnklm21@gmail.com>
2022-03-13 01:50:42 +0300

    Update deck.js

--- public/projects/js/deck/deck.js
+++ public/projects/js/deck/deck.js
@@ -1,6 +1,9 @@
 var suits = ['C', 'D', 'S', 'H']
-var ranks = ['1', '2', '3', '4', '5','6','7','8','8','10','J','Q','K','A']
+var ranks = ['2', '3', '4', '5','6','7','8','9','10','J','Q','K','A']
 
-for (suitCounter = 0; suitCounter < 4; suitCounter++){
-    console.log(suitCounter)
+for (suitCounter = 0; suitCounter < 13; suitCounter++){
+    console.log(ranks[suitCounter])
+}
+for (suitCounter = 0; suitCounter < 4; suitCounter++) {
+    console.log(suits[suitCounter])
 }
