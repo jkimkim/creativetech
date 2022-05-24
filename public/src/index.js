a4f490d5776c8d78b434b282192adfb61b4e4a03
jkimkim <kimjohnklm21@gmail.com>
2022-05-24 17:22:27 +0300

    update

--- public/src/index.js
+++ public/src/index.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {firebaseapp} from 'firebase/app';
+
+import firestore from "firebase/firestore";
 
 import '../css/bootstrap.css'
 import '../style1.css'
@@ -23,8 +24,12 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { timestamp };
+export default firebaseApp.firestore;
 
 
 import { bro } from "../hello";
