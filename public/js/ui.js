5dde44b5ff61ba99b80a85a4825ac1aee150fee6
jkimkim <kimjohnklm21@gmail.com>
2022-05-25 00:19:23 +0300

    update

--- public/js/ui.js
+++ public/js/ui.js
@@ -408,3 +408,6 @@ segmentAngleRad = function (Xstart, Ystart, Xtarget, Ytarget, realOrWeb) {
 
 
 //sign in/ sign up ui
+
+export const txtEmail = document.querySelector("#email");
+export const txtPassword = document.querySelector("#password");
