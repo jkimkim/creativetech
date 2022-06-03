a4098b6fc44c8923981fb868998e2bbb740c121e
jkimkim <kimjohnklm21@gmail.com>
2022-06-03 21:46:50 +0300

    update

--- public/src/aboutme.js
+++ public/src/aboutme.js
@@ -1,9 +1,8 @@
 import '../assets/web/assets/mobirise-icons2/mobirise2.css'
 import '../assets/web/assets/mobirise-icons-bold/mobirise-icons-bold.css'
 import '../assets/web/assets/mobirise-icons/mobirise-icons.css'
-import '../assets/bootstrap/css/bootstrap.min.css'
-import '../assets/bootstrap/css/bootstrap-grid.min.css'
-import '../assets/bootstrap/css/bootstrap-reboot.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 import '../assets/animatecss/animate.css'
 import '../assets/dropdown/css/style.css'
 import '../assets/socicon/css/styles.css'
